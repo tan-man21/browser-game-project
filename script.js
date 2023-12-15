215f3b829555b71318c5422b439052cb067b585e
Tanner Rogers <trroger2@alumni.ncsu.edu>
2023-12-15 12:56:20 -0500

    created game slots and unique ids

---
+++ script.js
@@ -0,0 +1,24 @@
+//Establish Rows and Columns
+let rows = 6
+let columns = 7
+
+let board = []
+
+window.onload = async () => {
+    setUpGame()
+}
+
+function setUpGame() {
+    for(let i = 0; i < rows; i++){
+        let newRowDiv = document.createElement('div')
+        newRowDiv.classList = 'slot'
+        newRowDiv.id = `0 - ${[i]}`
+        document.getElementById('board').appendChild(newRowDiv)
+        for(let j = 1; j < columns; j++){
+            let newColDiv = document.createElement('div')
+            newColDiv.classList = 'slot'
+            newColDiv.id = `${[i]} - ${[j+1]}`
+            document.getElementById('board').appendChild(newColDiv)
+        }
+    }
+}
