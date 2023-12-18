c7a76f1a027302422b02e9914bd9654961ab1992
Tanner Rogers <trroger2@alumni.ncsu.edu>
2023-12-18 12:32:43 -0500

    recognized coordinates on click

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
 //Establish Rows and Columns
-let rows = 6
-let columns = 7
+let rows = 6;
+let columns = 7;
+let playerRed = 'R';
+let playerBlack = 'B';
+let currentPlayer = getCurrentPlayer();
 
-let board = []
+let board;
+let gameOver = false;
+
 
 window.onload = async () => {
     setUpGame()
 }
-function selectPlayer(){
+async function selectPlayer(){
     let gameBoard = document.getElementById('board')
     let startContainer = document.getElementById('start_container')
     gameBoard.style.opacity = 1
+    await getCurrentPlayer()
     startContainer.remove()
-
 }
 
 function setUpGame() {
-    for(let i = 0; i < rows; i++){
-        let newRowDiv = document.createElement('div')
-        newRowDiv.classList = 'slot'
-        newRowDiv.id = `0 - ${[i]}`
-        document.getElementById('board').appendChild(newRowDiv)
-        for(let j = 1; j < columns; j++){
-            let newColDiv = document.createElement('div')
-            newColDiv.classList = 'slot'
-            newColDiv.id = `${[i]} - ${[j+1]}`
-            document.getElementById('board').appendChild(newColDiv)
+    board = []
+    for(let r = 0; r < rows; r++){
+        let row = [];
+        for(let c = 0; c < columns; c++){
+            row.push(' ');
+
+            let slot = document.createElement('div');
+            slot.id = r.toString() + '-' + c.toString();
+            slot.classList.add('slot');
+            slot.addEventListener('click', playerTurn)
+            document.getElementById('board').append(slot)
+        }
+        board.push(row);
+    }
+    console.log(board)
+}
+
+//get current player
+function getCurrentPlayer() {
+    let ele = document.getElementsByName('playerSelection');
+
+    for (let i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            if(ele[i].value === 'redPlayer'){
+                currentPlayer = playerRed
+            } else if(ele[i].value === 'blackPlayer'){
+                currentPlayer = playerBlack
+            }
         }
     }
+}
+
+function playerTurn(){
+    if(gameOver){
+        return;
+    }
+
+    let coords = this.id.split('-');
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    console.log(r + ',' + c)
 }
