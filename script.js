6405651071fc0b064677945ca49e6fa5bb22b83b
Tanner Rogers <trroger2@alumni.ncsu.edu>
2023-12-21 09:19:01 -0500

    started winning logic

--- script.js
+++ script.js
@@ -116,7 +116,26 @@ function playerTurn(){
 }
 //Checking for winner function
 function checkWinner(){
+    for(let r = 0; r < rows; r++){
+        for(let c = 0; c < columns - 3; c++){
+            if(board[r][c] != ' '){
+                if(board[r][c] == board[r][c+1] && board[r][c+1] == board[r][c+2] && board[r][c+2] == board[r][c+3]){
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+}
 
+function setWinner(r, c){
+    if(board[r][c] == playerRed){
+        alert('Red Player Wins!')
+    }else{
+        alert('Yellow Player Wins!')
+    }
+
+    gameOver = true;
 }
 
 //clear board with restart button
